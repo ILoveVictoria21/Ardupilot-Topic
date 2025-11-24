cea99eadfa5082d804e0668005a853ff3b64942b
BVLGARISSK <105140339+ILoveVictoria21@users.noreply.github.com>
2025-11-24 21:04:55 +0800

    Update and rename GPU.py to Test every feature one by one/GPU.py

--- Test every feature one by one/GPU.py
+++ Test every feature one by one/GPU.py
@@ -3,4 +3,5 @@ import torch
 print("CUDA 可用嗎:", torch.cuda.is_available())
 print("使用的 GPU:", torch.cuda.get_device_name(0))
 print("CUDA 版本:", torch.version.cuda)
+
 print("cuDNN 版本:", torch.backends.cudnn.version())    
